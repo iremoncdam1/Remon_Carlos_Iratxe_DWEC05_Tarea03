30c254e54d25d24ed09af53b527e3dee541220fd
Iratxe <iremoncdam1@gmail.com>
2024-03-14 19:17:24 +0100

    Proyecto terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import Articulo from './articulo.jsx'
-import data from './data.js'; 
-import { useState } from "react";
+import Articulo from './articulo.jsx'   //El componente "Articulo"
+import data from './data.js';   //El archivo en el que está la información
+import { useState } from "react";   //El import del hook useState
 
-//Creamos el elemento Titulo 
+//Creamos el elemento App 
 function App() {
+    //Se hace una variable contador (counter) y se inicializa a 1.
     const [counter, setCounter ] = useState(1);
 
+    //Usamos "setTimeOut" para que cada dos segundos vaya actualizando en contador.
+    //Si el contador ya es mayor que el número de elementos se vuleve a poner a 1.
     setTimeout(() => {
         let cont = counter>=data.length ? 1 : (counter + 1);
         setCounter(cont);
     }, 2000);
 
+    //Se hace el primer componente y desde la etiqueta "section" se incluye el componente "Articulo" que es el que tendrá la información
     return (
         <section className="section">
             <div className="title">
--- src/articulo.jsx
+++ src/articulo.jsx
@@ -1,13 +1,13 @@
 import data from './data.js'; 
 
 function Articulo({index}) {
-    //TODO:: Que hay que hacer cuando ya estamos en el último articulo
     return (   
         <div className="section-center">
             {
+                //Por cada elemento se crea un "article" con la imagen específica, un nombre y la categoría.
                 data.map(({id, img, categoria, src }) => (
-                 
-                    <article key={id} className={ ((id==index) ? 'activeSlide' : (id < index) ? "lastSlide" : 'nextSlide')}>
+                    //Dependiendo del id del articulo y del index pasado desde el primer componente, se le pone al articulo una clase u otra
+                    <article key={id} className={ ((id==index) ? 'activeSlide' : (id > index) ? "nextSlide" : 'lastSlide')}>
                         <img src={src} alt={img} className="person-img"></img>
                         <h4>{img}</h4>
                         <p className="title">{categoria}</p>
